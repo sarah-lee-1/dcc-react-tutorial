575f0e34778f804f36d123647b5b01d15de715c8
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-01-04 21:11:48 -0600

    Added New Entry Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+import AddEntryForm from './Components/AddEntryForm/AddEntryForm';
 
 function App() {
 
 const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}, {weight: 176, date: '11-24-2021'}])
 
+  function addNewEntry(entry) {
+
+    let tempEntries = [...entries, entry];
+
+    setEntries(tempEntries);
+  }
+
   return (
     <div >
       <DisplayEntries parentEntries={entries} /> 
+      <AddEntryForm addNewEntryProperty={AddNewEntry}/> 
+
     </div>
   );
 }
